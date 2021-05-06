6bf1dbef0641568dc48d5dca6f3405538ec503f3
John Maindonald <john@statsresearch.co.nz>
2021-05-06 21:36:46 +1200

    Fix errors in extractLT(), and in the vignettes timeMortslity.Rmd and MeBrPlot.Rmd

--- R/extractLT.R
+++ R/extractLT.R
@@ -1,22 +1,37 @@
 #' Obtain complete set of LT or LD estimates
 #'
-#' @param p Desired mortality proportion.
+#' @description When supplied with a model object that has fitted
+#' dose-response lines for each of several levels of a factor,
+#' `extractLR` calls the function `fieller` to calculate lethal time
+#` or lethal dose or other such estimates.
+#'
+#' @details Fixed coefficients from `obj` must be for intercepts and
+#' for slopes.  Starting the model formula with `0+` will commonly
+#' do what is required. The coefficients `fixef(obj)[a]` are assumed
+#' to specify line intercepts, while `fixef(obj)[b]` specify the
+#' corresponding slopes.  These replace the arguments `nEsts`
+#' (subscripts for intercepts were `1:nEsts`) and `slopeAdd`
+#' (subscripts for slopes were `(nEsts+1):(nEsts+slopeAdd)`).
+#'
+#' @param p Target mortality proportion.
 #' @param obj \code{merMod} object, created using \code{lmer()} or
-#' \code{glmer()}.
+#' \code{glmerMod} object, created using \code{glmer()}.
 #' @param link Link function, specified as a character string.
 #' For a folded power function, specify \code{"fpower"}.
 #' @param logscale Logical.  Specify \code{TRUE}, if LT values are
 #' to be back-transformed from a logarithmic scale.
-#' @param nEsts Number of groups. The first \code{nEsts} estimates
-#' are assumed to be intercepts.
-#' @param slopeAdd Add to the intercept number to get corresponding slope.
+#' @param a Subscripts for intercepts.
+#' @param b Subscripts for corresponding slopes.
 #' @param lambda (\code{extractLTpwr} only) Power for power function.
 #' @param eps Replace \code{prob} by \code{prob+eps} before transformation.
 #' @param offset Use to undo scaling of time or dose variable. This is
 #' passed to the \code{\link{fieller}} function that \code{extractLT}
 #' calls.
 #' @param df.t Degrees of freedom for variance-covariance matrix.
-#' If not supplied, it will be calculated internally.
+#' If not supplied, it will be calculated internally, using a
+#' conservative (low) value.  One is using the \code{t} distribution as
+#' an approximation.  Getting a good approximation is not straightforward.
+#' See `vignette('introduction-mixed-models', package='afex')`, page 19.
 #'
 #' @return Matrix holding LD or LD estimates.
 #' @export
@@ -32,11 +47,12 @@ extractLT <-
            obj,
            link="logit",
            logscale=FALSE,
-           nEsts=8,
-           slopeAdd=8,
+           a=1:3,
+           b=4:6,
            eps=0,
            offset=0,
            df.t=NULL){
+  if(length(a)!=length(b))stop("`a` and `b` must be the same length")
   if(inherits(obj,'lm')){
     if(is.null(df.t))df.t <- summary(obj)$df.residual
     bfun <- coef
@@ -54,17 +70,17 @@ extractLT <-
     print(paste("No provision for object of class",cl))
     return()
   }
-  LT99 <- matrix(0, nrow=nEsts, ncol=4)
+  LT99 <- matrix(0, nrow=length(a), ncol=4)
   pAdj <- (p+eps)/(1+2*eps)
   blmm <- bfun(obj)
   vlmm <- varfun(obj)
-  rownames(LT99) <- names(blmm)[1:nEsts]
-  if(is.null(df.t))df.t <- ngrps-nEsts
-  for(i in 1:nEsts){
-    ab <- c(i, slopeAdd+i)
+  rownames(LT99) <- names(blmm)[1:length(a)]
+  if(is.null(df.t))df.t <- ngrps[length(ngrps)]-1
+  for(i in 1:length(a)){
+    ab <- c(a[i], b[i])
     bi <- blmm[ab]
     vii <- vlmm[ab,ab]
-    LT99[i,] <- qra::fieller(pAdj, bi,vii, df.t=df.t, offset=offset,
+    LT99[i,] <- qra::fieller(pAdj, bi, vii, df.t=df.t, offset=offset,
                              logscale=logscale, link=link, eps=eps)[1:4]
   }
   colnames(LT99) <- c("est", "var", "lwr", "upr")
@@ -79,18 +95,19 @@ extractLTpwr <-
                       obj,
                       link="fpower",
                       logscale=FALSE,
-                      nEsts=8,
-                      slopeAdd=8,
+                      a=1:3,
+                      b=1:3,
                       lambda=0,
                       eps=0.015,
                       offset=0,
                       df.t=NULL){
-  LT99 <- matrix(0, nrow=nEsts, ncol=4)
-
-  rownames(LT99) <- names(fixef(obj))[1:nEsts]
-  if(is.null(df.t))df.t <- summary(obj)$ngrps-nEsts-1
-  for(i in 1:nEsts){
-    ab <- c(i, slopeAdd+i)
+  if(length(a)!=length(b))stop("`a` and `b` must be the same length")
+  LT99 <- matrix(0, nrow=length(a), ncol=4)
+  rownames(LT99) <- names(fixef(obj))[1:length(a)]
+  ngrps <- summary(obj)$ngrps
+  if(is.null(df.t))df.t <- ngrps[length(ngrps)]-1
+  for(i in 1:length(a)){
+    ab <- c(a[i], b[i])
     blmm <- coef(summary(obj))[ab]
     vlmm <- vcov(obj)[ab,ab]
     LT99[i,] <- qra::fieller2(p, blmm,vlmm, link="fpower",
